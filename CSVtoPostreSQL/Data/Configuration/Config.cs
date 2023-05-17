5ccfcb75654e3a11fc01833707285d201eae0b79
Stoyan Gospodinov <sonic@mail.bg>
2023-05-17 20:42:44 +0300

    Reworking

--- CSVtoPostreSQL/Data/Configuration/Config.cs
+++ CSVtoPostreSQL/Data/Configuration/Config.cs
@@ -5,7 +5,7 @@ namespace CSVtoPostreSQL.Data.Configuration
     public class Config
     {
 
-        public static string ConnectionString = @"Host=localhost; Database=Offers; Username=postgres; Password=Blqsu4ko";
+        public static string ConnectionString = "";
 
 
     }
